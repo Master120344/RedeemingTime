fb6e375d601b16962aea2e572ff9a56cc5f78eaf
JT <Master120344@yahoo.com>
2024-08-04 22:49:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,15 @@ function openTab(evt, tabName) {
     // Remove the active class from all tab links
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab content, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Set default tab to open
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.tablinks').click();
+});
